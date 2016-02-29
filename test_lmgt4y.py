1304613d6ead2303722b606a28f523748836a5a3
nesh69 <nesh69@gmail.com>
2016-02-29 03:32:53 +0300

    SE builder tests update
    SE builder tests update

--- test_lmgt4y.py
+++ test_lmgt4y.py
@@ -10,24 +10,24 @@ def is_alert_present(wd):
     except:
         return False
 
-class (unittest.TestCase):
+class test_lmgt4y(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_(self):
+    def test_test_lmgt4y(self):
         success = True
         wd = self.wd
-        wd.get("https://www.google.ru/?gfe_rd=cr&ei=FMG9VpzXLY3DNLCqpaAE&gws_rd=ssl")
-        wd.find_element_by_id("lst-ib").click()
-        wd.find_element_by_id("lst-ib").clear()
-        wd.find_element_by_id("lst-ib").send_keys("let me google that")
-        wd.find_element_by_css_selector("b").click()
-        wd.find_element_by_name("btnG").click()
-        wd.find_element_by_name("btnG").click()
-        wd.find_element_by_link_text("Let me google that for you").click()
-        wd.find_element_by_link_text("×").click()
-        wd.find_element_by_id("rcnt").click()
+        wd.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+        wd.find_element_by_id("searchInput").click()
+        wd.find_element_by_id("searchInput").clear()
+        wd.find_element_by_id("searchInput").send_keys("олененок")
+        wd.find_element_by_id("searchButton").click()
+        wd.find_element_by_link_text("Оленёнок Рудольф").click()
+        wd.find_element_by_link_text("3 Примечания").click()
+        wd.find_element_by_link_text("Вячеслав Баранов").click()
+        wd.find_element_by_link_text("5.1 Роли в кино").click()
+        wd.find_element_by_link_text("Ранетки").click()
         self.assertTrue(success)
     
     def tearDown(self):
